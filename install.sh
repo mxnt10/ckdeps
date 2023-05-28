ad6f4b73410cfeab954d0209f90b786b5a4fcdba
mxnt10 <m10ferrari1200@gmail.com>
2023-05-28 13:54:25 -0300

    add changelog

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ install_root=${install_root:-""}
 
 set -e
 [ "$install_root" != "" ] && {
-  mkdir -p "$install_root"/usr/{bin,share/applications,doc/"$PRGNAM"-"$VERSION"}
+  mkdir -p "$install_root"/usr/{bin,share/applications,doc/"$PRGNAM-$VERSION"}
 } || {
-  mkdir -p /usr/doc/"$PRGNAM"-"$VERSION"
+  mkdir -p "/usr/doc/$PRGNAM-$VERSION"
 }
 
-install -Dm 0755 "$PRGNAM" "$install_root"/usr/bin
-cp -a RELEASE LICENSE README.md "$install_root"/usr/doc/"$PRGNAM"-"$VERSION"
+install -Dm 0755 "$PRGNAM" "$install_root/usr/bin"
+cp -a changelog RELEASE LICENSE README.md "$install_root/usr/doc/$PRGNAM-$VERSION"
 
 exit 0
